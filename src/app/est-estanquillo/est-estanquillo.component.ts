1315001ea1388dc24e3b33274a8eab08d0b1a761
Pipe716 <pipe716@hotmail.com>
2016-11-05 15:08:02 -0500

    Version inicila del proyecto

---
+++ src/app/est-estanquillo/est-estanquillo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-est-estanquillo',
+  templateUrl: './est-estanquillo.component.html',
+  styleUrls: ['./est-estanquillo.component.css']
+})
+export class EstEstanquilloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
